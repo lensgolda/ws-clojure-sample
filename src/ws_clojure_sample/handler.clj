8f25cd343d5c59085655957922f14420b993edd6
Lens <lensgolda@gmail.com>
2018-10-07 15:31:51 +0200

    [*] fix intends;

--- src/ws_clojure_sample/handler.clj
+++ src/ws_clojure_sample/handler.clj
@@ -6,8 +6,8 @@
 (defn ws-handler
   "Main WebSocket handler"
   [request]                                                              ;; Принимает запрос
-    (with-channel request channel                                          ;; Получает канал
-      (swap! clients assoc channel true)                                   ;; Сохраняем пул клиентов с которыми установлено соединение в атом clients и ставим флаг true
-      (println channel "Connection established")
-      (on-close channel (fn [status] (println "channel closed: " status))) ;; Устанавливает обработчик при закрытии канала
-      (on-receive channel (get receiver :chat))))                          ;; Устаналивает обработчик данных из канала (его создадим далее)
+  (with-channel request channel                                          ;; Получает канал
+    (swap! clients assoc channel true)                                   ;; Сохраняем пул клиентов с которыми установлено соединение в атом clients и ставим флаг true
+    (println channel "Connection established")
+    (on-close channel (fn [status] (println "channel closed: " status))) ;; Устанавливает обработчик при закрытии канала
+    (on-receive channel (get receiver :chat))))                          ;; Устаналивает обработчик данных из канала (его создадим далее)
--- src/ws_clojure_sample/receiver.clj
+++ src/ws_clojure_sample/receiver.clj
@@ -13,18 +13,18 @@
     "Chat receiver"
     [data]
     (doseq [client (keys @clients)]
-            (send! client (json/write-str {:key "chat" :data data})))) ;; example: return received data back to client
+      (send! client (json/write-str {:key "chat" :data data})))) ;; example: return received data back to client
 
 ;; on-receive channel handler, for app data getter example
 (defn data-receiver
     "Data receiver"
     [data]
     (let [responses (map #(future (slurp %)) urls)]
-        (doall (map (fn [resp]
-          (doseq [client (keys @clients)]
-            (send! client @resp))) responses))))
+      (doall (map (fn [resp]
+                    (doseq [client (keys @clients)]
+                      (send! client @resp))) responses))))
 
 ;; Receivers map, for WebSocket handler (see ws-handler)
 ;; for new one, write new receiver and add it to map
 (def receiver {:chat chat-receiver
-               :data data-receiver })
+               :data data-receiver})
